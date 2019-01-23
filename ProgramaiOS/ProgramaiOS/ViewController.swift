7fb011fcf8f3720ce38ebff9bb4f04b3a1322358
iancupgop <ian18charles@gmail.com>
2019-01-23 09:44:50 -0600

    ComentarioUno
    Copie el projecto de xcode de ExampleiOS

---
+++ ProgramaiOS/ProgramaiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgramaiOS
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 23/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
